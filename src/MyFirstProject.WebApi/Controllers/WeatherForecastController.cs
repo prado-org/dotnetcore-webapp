ecb9d139a1906b5c1502de1f912e74e7f490f785
Leandro Prado <69152421+leandromsft@users.noreply.github.com>
2024-03-05 19:23:06 +0000

    Refactor database connection in WeatherForecastController.cs

--- src/MyFirstProject.WebApi/Controllers/WeatherForecastController.cs
+++ src/MyFirstProject.WebApi/Controllers/WeatherForecastController.cs
@@ -69,7 +69,8 @@ namespace MyFirstProject.WebApi.Controllers
         {
             try
             {
-                using SqlConnection connection = new SqlConnection("Server=localhost;Database=Todo;User Id=sa;Password=Password123;");
+                string connectionString = _configuration.GetConnectionString("DefaultConnection");
+                using SqlConnection connection = new SqlConnection(connectionString);
                 connection.OpenAsync();
 
                 string deleteCommand = "DELETE FROM WeatherForecast WHERE id = " + id.ToString();
