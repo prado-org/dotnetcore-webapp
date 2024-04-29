a8dbb45ce4c0ab7edc3123fcb3bb099ab77501fc
Leandro Prado <leadro@microsoft.com>
2024-04-29 12:52:06 -0300

    Adicionar métodos para exclusão e criação de itens na API TodoItemController.cs

--- src/MyFirstProject.WebApi/Controllers/TodoItemController.cs
+++ src/MyFirstProject.WebApi/Controllers/TodoItemController.cs
@@ -25,5 +25,47 @@ namespace MyFirstProject.WebApi.Controllers
             _logger.LogInformation("Method - GetTodoItems");
             return await _context.TodoItems.ToListAsync();
         }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }        /// ///         _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        //criar um método para adicionar um item e validar se o nome contem caractet especial 
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        {
+            _logger.LogInformation("Method - PostTodoItem");
+            if (todoItem.Name.Contains("@"))
+            {
+                return BadRequest();
+            }
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+        }
+
+        // GET: api/TodoItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+        
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+        
+            return todoItem;
+        }
     }
 }
