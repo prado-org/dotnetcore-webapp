3f6ab5f0b11a0e2bb7b1e14c6f92bad5045b37d5
Leandro Prado <leadro@microsoft.com>
2024-06-11 15:24:44 -0300

    feat: Add endpoint to retrieve a specific TodoItem by ID
    The code changes in the `TodoItemController.cs` file add a new HTTP GET endpoint that retrieves a specific TodoItem by its ID. This endpoint queries the database and returns the corresponding TodoItem object.
    
    This commit message follows the established convention of starting with a type prefix (`feat`) to indicate the nature of the changes. It provides a clear and concise description of the purpose of the code changes.

--- src/MyFirstProject.WebApi/Controllers/TodoItemController.cs
+++ src/MyFirstProject.WebApi/Controllers/TodoItemController.cs
@@ -25,5 +25,34 @@ namespace MyFirstProject.WebApi.Controllers
             _logger.LogInformation("Method - GetTodoItems");
             return await _context.TodoItems.ToListAsync();
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            _logger.LogInformation($"Method - GetTodoItem: {id}");
+
+            TodoItem item = null;
+            using SqlConnection connection = new SqlConnection("Server=localhost;Database=Todo;User Id=sa;Password=Password123;");
+            connection.OpenAsync();
+            
+            string selectCommand = "SELECT * FROM TodoItem WHERE id = " + id.ToString();
+
+            SqlCommand command = new SqlCommand(selectCommand, connection);
+
+            SqlDataReader reader = command.ExecuteReader();
+            
+            while (reader.Read())
+            {
+                long _id = reader.GetInt16(0);
+                string _name = reader.GetString(1);
+                bool _complete = reader.GetBoolean(2);
+
+                item = new TodoItem { Id = _id, Name = _name, IsComplete = _complete };
+            }
+
+            return item;
+        }
+
+
     }
 }
