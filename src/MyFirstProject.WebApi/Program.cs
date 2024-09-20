4c146b7ad8afba4126cf14b2277c9d8e1a4d7cee
Leandro Prado <leadro@microsoft.com>
2024-09-20 13:12:07 -0300

    Refactor Swagger configuration

--- src/MyFirstProject.WebApi/Program.cs
+++ src/MyFirstProject.WebApi/Program.cs
@@ -37,9 +37,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // removido por causa do Codespaces
 //app.UseHttpsRedirection();
