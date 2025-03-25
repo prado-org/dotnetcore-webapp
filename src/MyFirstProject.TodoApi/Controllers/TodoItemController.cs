dd26906f922494e41d67a4ceb7add043507b32c2
Leandro Prado <leadro@microsoft.com>
2025-03-25 08:23:55 -0300

    feat: refatorar controle de itens e previsão do tempo
    Removido código não utilizado do controlador TodoItemController e adicionado método CreateXml no WeatherForecastController.
    Implementada a recuperação de previsões do tempo a partir do banco de dados, utilizando SqlConnection e SqlCommand.
    Adicionada referência ao pacote Microsoft.EntityFrameworkCore.SqlServer no projeto WeatherForecastApi.

--- src/MyFirstProject.TodoApi/Controllers/TodoItemController.cs
+++ src/MyFirstProject.TodoApi/Controllers/TodoItemController.cs
@@ -1,8 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using MyFirstProject.TodoApi.Models;
-using System.Xml;
+
 
 namespace MyFirstProject.TodoApi.Controllers
 {
@@ -24,51 +23,6 @@ namespace MyFirstProject.TodoApi.Controllers
         {
             _logger.LogInformation("Method GetTodoItems");
             return await _context.TodoItems.ToListAsync();
-        }
-
-        private void CreateXml(string itemName)
-        {
-            _logger.LogInformation("Method CreateXml");
-            using (XmlWriter writer = XmlWriter.Create("todos.xml"))
-            {
-                writer.WriteStartDocument();
-                writer.WriteRaw("<todo><name>" + itemName + "</name></todo>");
-                writer.WriteEndElement();
-                writer.WriteEndDocument();
-            }
-        }
-
-        private TodoItem GetTodoItemById(int id)
-        {
-            _logger.LogInformation("Method GetTodoItemById");
-            try
-            {
-                TodoItem item = null;
-                using SqlConnection connection = new SqlConnection("Server=localhost;Database=Todo;User Id=sa;Password=Password123;");
-                connection.OpenAsync();
-
-                string selectCommand = "SELECT * FROM TodoItem WHERE id = " + id.ToString();
-
-                SqlCommand command = new SqlCommand(selectCommand, connection);
-
-                SqlDataReader reader = command.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    int Id = reader.GetInt32(0);
-                    string Name = reader.GetString(1);
-                    bool IsComplete = reader.GetBoolean(2);
-                    DateTime DueDate = reader.GetDateTime(3);
-
-                    item = new TodoItem { Id = Id, Name = Name, IsComplete = IsComplete, DueDate = DueDate };
-                }
-
-                return item;
-            }
-            catch (Exception)
-            {
-                throw;
-            }
         }
     }
 }
--- src/MyFirstProject.WeatherForecastApi/Controllers/WeatherForecastController.cs
+++ src/MyFirstProject.WeatherForecastApi/Controllers/WeatherForecastController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using MyFirstProject.WeatherForecastApi.Models;
+using Microsoft.Data.SqlClient;
+using System.Xml;
 
 namespace MyFirstProject.WeatherForecastApi.Controllers
 {
@@ -30,6 +32,53 @@ namespace MyFirstProject.WeatherForecastApi.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        private void CreateXml(string itemName)
+        {
+            _logger.LogInformation("Method CreateXml");
+            using (XmlWriter writer = XmlWriter.Create("todos.xml"))
+            {
+                writer.WriteStartDocument();
+                writer.WriteRaw("<todo><name>" + itemName + "</name></todo>");
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+            }
+        }
+
+        private WeatherForecast GetWeatherForecastById(int id)
+        {
+            _logger.LogInformation("Method GetTodoItemById");
+            try
+            {
+                WeatherForecast item = null;
+                using SqlConnection connection = new SqlConnection("Server=localhost;Database=Todo;User Id=sa;Password=Password123;");
+                connection.OpenAsync();
+
+                string selectCommand = "SELECT * FROM WeatherForecast WHERE id = " + id.ToString();
+
+                SqlCommand command = new SqlCommand(selectCommand, connection);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+
+                    item = new WeatherForecast
+                    {
+                        Date = reader.GetDateTime(1),
+                        TemperatureC = reader.GetInt32(2),
+                        Summary = reader.GetString(3)
+                    };
+                }
+
+                return item;
+            }
+            catch (Exception ex)    
+            {
+                _logger.LogError("Error in GetTodoItemById: " + ex.Message);
+                throw ex;
+            }
         }
     }
 }
